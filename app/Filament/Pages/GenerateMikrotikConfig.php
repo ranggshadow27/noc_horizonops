74341867435e8785ed0c67013b48f9307a1661d8
Rangga Tito Prayogo <ranggashadow27@gmail.com>
2025-08-18 12:52:30 +0700

    Tes GWN CFG

--- app/Filament/Pages/GenerateMikrotikConfig.php
+++ app/Filament/Pages/GenerateMikrotikConfig.php
@@ -316,7 +316,7 @@ class GenerateMikrotikConfig extends Page
                 $moveBinCommand = sprintf('cd "%s" && mv "%s" ../storage/app/temp', $binFolderPath, $outputFileName);
                 $moveBinCommandoutput = shell_exec($moveBinCommand . ' 2>&1');
 
-                dd($moveBinCommand);
+                // dd($moveBinCommand);
 
                 // Storage::delete('temp/' . $txtFileName);
                 $contentType = 'application/octet-stream';
