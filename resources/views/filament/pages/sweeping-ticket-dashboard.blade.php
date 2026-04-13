0349adc0333006f4193fdd4f7a95c4b4ebae3d7d
ranggashadow27 <ranggashadow27@gmail.com>
2026-04-13 12:09:15 +0700

    Update HaloBakti Resource

--- resources/views/filament/pages/sweeping-ticket-dashboard.blade.php
+++ resources/views/filament/pages/sweeping-ticket-dashboard.blade.php
@@ -20,42 +20,6 @@
         </div>
     </div>
 
-    <div class="flex flex-wrap justify-center items-center gap-4 w-full">
-        {{-- <div class="flex-1 sm:w-1/2">
-        @livewire(\App\Filament\Widgets\SweepingTicketUnWarningTableChart::class)
-    </div> --}}
-
-        <div class="flex-1 sm:w-1/2">
-            @livewire(\App\Filament\Widgets\SweepingTicketWarningTableChart::class)
-        </div>
-
-        <div class="flex-1 sm:w-1/2">
-            @livewire(\App\Filament\Widgets\SweepingTicketMinorTableChart::class)
-        </div>
-
-        <div class="flex-1 sm:w-1/2">
-            @livewire(\App\Filament\Widgets\SweepingTicketMajorTableChart::class)
-        </div>
-    </div>
-
-    <div class="flex flex-wrap justify-center items-center gap-4 w-full">
-        {{-- <div class="flex-1 sm:w-1/2">
-        @livewire(\App\Filament\Widgets\SweepingTTStatusChart::class)
-    </div> --}}
-
-        <div class="flex-1 sm:w-1/2">
-            @livewire(\App\Filament\Widgets\SweepingTicketWarningTrendChart::class)
-        </div>
-
-        <div class="flex-1 sm:w-1/2">
-            @livewire(\App\Filament\Widgets\SweepingTicketMinorTrendChart::class)
-        </div>
-
-        <div class="flex-1 sm:w-1/2">
-            @livewire(\App\Filament\Widgets\SweepingTicketMajorTrendChart::class)
-        </div>
-    </div>
-
 </x-filament-panels::page>
 
 <script>
