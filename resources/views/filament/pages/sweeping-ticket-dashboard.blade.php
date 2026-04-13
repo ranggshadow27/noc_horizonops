a18cf118d84136bf1bf959780c00c0c38f322ea6
ranggashadow27 <ranggashadow27@gmail.com>
2026-04-13 12:21:55 +0700

    Update HaloBakti Resource

--- resources/views/filament/pages/sweeping-ticket-dashboard.blade.php
+++ resources/views/filament/pages/sweeping-ticket-dashboard.blade.php
@@ -10,7 +10,7 @@
         </div>
     </div>
 
-    {{-- <div class="flex flex-wrap justify-center items-center gap-4 w-full">
+    <div class="flex flex-wrap justify-center items-center gap-4 w-full">
         <div class="flex-1 sm:w-1/2">
             @livewire(\App\Filament\Widgets\SweepingTTOverview::class)
         </div>
@@ -18,7 +18,7 @@
         <div class="flex-1 sm:w-1/2">
             @livewire(\App\Filament\Widgets\SecondSweepingTTOverview::class)
         </div>
-    </div> --}}
+    </div>
 
     <div class="flex flex-wrap justify-center items-center gap-4 w-full">
         <div class="flex-1 sm:w-1/2">
@@ -49,34 +49,3 @@
     </div>
 
 </x-filament-panels::page>
-
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        function updateClock() {
-            const now = new Date();
-
-            // Format date in Indonesian
-            const optionsDate = {
-                weekday: 'long',
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric'
-            };
-            const formattedDate = now.toLocaleDateString('id-ID', optionsDate);
-
-            // Get hours, minutes and seconds separately
-            const hours = String(now.getHours()).padStart(2, '0'); // Ensure two digits
-            const minutes = String(now.getMinutes()).padStart(2, '0'); // Ensure two digits
-            const seconds = String(now.getSeconds()).padStart(2, '0'); // Ensure two digits
-
-            // Combine them using ':' as separator for 24-hour format
-            const formattedTime = `${hours}:${minutes}:${seconds}`;
-
-            document.getElementById('realTimeClock').textContent = `${formattedDate} - ${formattedTime} WIB`;
-        }
-
-        // Update the clock immediately and then every second
-        updateClock();
-        setInterval(updateClock, 1000);
-    });
-</script>
