eb80ad8a81d736a9c3709be767a88d425066cd0e
ranggashadow27 <ranggashadow27@gmail.com>
2026-04-13 12:27:13 +0700

    Adjust Sweeping Ticket

--- resources/views/filament/pages/sweeping-ticket-dashboard.blade.php
+++ resources/views/filament/pages/sweeping-ticket-dashboard.blade.php
@@ -10,7 +10,43 @@
         </div>
     </div>
 
+    <div class="flex flex-wrap justify-center items-center gap-4 w-full">
+        <div class="flex-1 sm:w-1/2">
+            @livewire(\App\Filament\Widgets\SweepingTTOverview::class)
+        </div>
 
+        <div class="flex-1 sm:w-1/2">
+            @livewire(\App\Filament\Widgets\SecondSweepingTTOverview::class)
+        </div>
+    </div>
+
+    <div class="flex flex-wrap justify-center items-center gap-4 w-full">
+        <div class="flex-1 sm:w-1/2">
+            @livewire(\App\Filament\Widgets\SweepingTicketWarningTableChart::class)
+        </div>
+
+        <div class="flex-1 sm:w-1/2">
+            @livewire(\App\Filament\Widgets\SweepingTicketMinorTableChart::class)
+        </div>
+
+        <div class="flex-1 sm:w-1/2">
+            @livewire(\App\Filament\Widgets\SweepingTicketMajorTableChart::class)
+        </div>
+    </div>
+
+    <div class="flex flex-wrap justify-center items-center gap-4 w-full">
+        <div class="flex-1 sm:w-1/2">
+            @livewire(\App\Filament\Widgets\SweepingTicketWarningTrendChart::class)
+        </div>
+
+        <div class="flex-1 sm:w-1/2">
+            @livewire(\App\Filament\Widgets\SweepingTicketMinorTrendChart::class)
+        </div>
+
+        <div class="flex-1 sm:w-1/2">
+            @livewire(\App\Filament\Widgets\SweepingTicketMajorTrendChart::class)
+        </div>
+    </div>
 
 </x-filament-panels::page>
 
